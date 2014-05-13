0f711ba204e5321f067f145512082ad8adc69fbb
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2014-05-13 22:19:12 +0200

    Cleaning

--- src/main/java/com/twasyl/lat/scene/controls/slide/SlideElement.java
+++
@@ -1,9 +0,0 @@
-package com.twasyl.lat.scene.controls.slide;
-
-import javafx.beans.property.ObjectProperty;
-
-public interface  SlideElement<T> {
-    ObjectProperty<T> referenceLayoutElementProperty();
-    T getReferenceLayoutElement();
-    void setReferenceLayoutElement(T referenceLayoutElement);
-}

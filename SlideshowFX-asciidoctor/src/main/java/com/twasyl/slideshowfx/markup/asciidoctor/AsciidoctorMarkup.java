2e69a5db5542dea447c2a1cb7ba07264f8f77f04
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2016-01-07 12:37:32 +0100

    Upgrade to asciidoctorj 1.5.4

--- SlideshowFX-asciidoctor/src/main/java/com/twasyl/slideshowfx/markup/asciidoctor/AsciidoctorMarkup.java
+++ SlideshowFX-asciidoctor/src/main/java/com/twasyl/slideshowfx/markup/asciidoctor/AsciidoctorMarkup.java
@@ -52,7 +52,7 @@ public class AsciidoctorMarkup extends AbstractMarkup {
         RubyInstanceConfig config = new RubyInstanceConfig();
         config.setLoader(AsciidoctorMarkup.class.getClassLoader());
 
-        JavaEmbedUtils.initialize(Arrays.asList("META-INF/jruby.home/lib/ruby/2.0", "gems/asciidoctor-1.5.3/lib"), config);
+        JavaEmbedUtils.initialize(Arrays.asList("META-INF/jruby.home/lib/ruby/2.0", "gems/asciidoctor-1.5.4/lib"), config);
 
         this.asciidoctor = Asciidoctor.Factory.create(AsciidoctorMarkup.class.getClassLoader());
     }

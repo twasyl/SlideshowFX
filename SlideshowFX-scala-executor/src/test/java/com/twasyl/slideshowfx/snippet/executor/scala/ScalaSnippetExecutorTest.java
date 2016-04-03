15280968a528cef018a97a08d0b66b6d09144104
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2016-04-03 17:35:31 +0200

    Refactor of Scala plugin.

---
+++ SlideshowFX-scala-executor/src/test/java/com/twasyl/slideshowfx/snippet/executor/scala/ScalaSnippetExecutorTest.java
@@ -0,0 +1,9 @@
+package com.twasyl.slideshowfx.snippet.executor.scala;
+
+/**
+ * @author Thierry Wasylczenko
+ * @version 1.0
+ * @since SlideshowFX 1.0.0
+ */
+public class ScalaSnippetExecutorTest {
+}

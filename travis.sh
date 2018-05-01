9cafd93f9790d073cf3298586dde581bfec77266
Thierry Wasylczenko <thierry.wasylczenko@gmail.com>
2018-05-01 14:06:29 +0200

    - Update to JUnit 5.2.0 - Update JDK for travis

--- travis.sh
+++ travis.sh
@@ -1,15 +1,14 @@
-echo "Setup JDK 1.8.0_162"
-jdkVersion=1.8.0_162
+echo "Setup JDK 1.8.0_172"
+jdkVersion=1.8.0_172
 jdkInstallationFolder=jdk$jdkVersion
-jdkBuild=$jdkVersion-b12
-jdkArchive=jdk-8u162-linux-x64.tar.gz
+jdkArchive=jdk-8u172-linux-x64.tar.gz
 
 mkdir -p ~/jvm
 pushd ~/jvm > /dev/null
 
 if [ ! -d "$jdkInstallationFolder" ]; then
 	echo "Downloading JDK $jdkVersion"
-	wget --quiet --continue --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/$jdkArchive
+	wget --quiet --continue --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/$jdkArchive
 	tar xzf $jdkArchive
 	rm $jdkArchive
 	pushd $jdkInstallationFolder > /dev/null
